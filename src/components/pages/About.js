6ebed0c6de4366399f19d18b540413d8a26a2082
ayesrab <ayesrab791@gmail.com>
2020-08-11 02:39:17 +0530

    First Commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        This is the TodoList app v1.1.0.This is a part of React crash course{" "}
+      </p>
+    </React.Fragment>
+  );
+}
+
+export default About;
